9a15aafa92616e9a64f3a7d9c65b43e8b09a72e3
Jordi Prats <jprats@systemadmin.es>
2016-12-01 10:32:26 +0100

    sql thin client install i386

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,5 +1,11 @@
 class oracleclient::params {
 
+    # sql thin client install i386
+    #
+    # rpm -ihv https://s3.eu-central-1.amazonaws.com/ar-ecd9f97f164d1a813fb4705c806208d2/ar-oraclient-rpm/oic-basic.rpm
+    # rpm -ihv https://s3.eu-central-1.amazonaws.com/ar-ecd9f97f164d1a813fb4705c806208d2/ar-oraclient-rpm/oic-devel.rpm
+    # rpm -ihv https://s3.eu-central-1.amazonaws.com/ar-ecd9f97f164d1a813fb4705c806208d2/ar-oraclient-rpm/oic-sqlplus.rpm
+
   case $::osfamily
   {
     'redhat':
